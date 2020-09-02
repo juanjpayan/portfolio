6df0a590bbdfdb2fdea6386ad113717cab926cb0
jpjuansito <jpjuansito@penguin>
2020-09-01 22:33:10 -0400

    updated contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,74 +1,62 @@
-import React from "react";
-import Helmet from "react-helmet";
+import React from 'react';
+import Helmet from 'react-helmet';
 
-import Layout from "components/Layout";
-import Container from "components/Container";
+import Layout from 'components/Layout';
+import Container from 'components/Container';
 
-import mail from "assets/images/SVG/mail.svg";
-import linked from "assets/images/SVG/linkedin.svg";
-import github from "assets/images/SVG/github.svg";
-import dribbble from "assets/images/SVG/dribbble.svg";
-import insta from "assets/images/SVG/instagram.svg";
+import mail from 'assets/images/SVG/mail.svg';
+import linked from 'assets/images/SVG/linkedin.svg';
+import github from 'assets/images/SVG/github.svg';
+import dribbble from 'assets/images/SVG/dribbble.svg';
+import insta from 'assets/images/SVG/instagram.svg';
 
 const ContactPage = () => {
-  return (
-    <Layout pageName="contact">
-      <Helmet>
-        <title>Contact</title>
-      </Helmet>
-      <Container>
-        <h1>Contact</h1>
-        <p>Let's get in touch!</p>
-        <div className="contact__icon">
-          <a
-            href="https://www.linkedin.com/in/arslaanamar/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg className="contact__icon-item">
-              <use xlinkHref={`#${linked.id}`} />
-            </svg>
-          </a>
+	return (
+		<Layout pageName="contact">
+			<Helmet>
+				<title>Contact</title>
+			</Helmet>
+			<Container>
+				<h1>Contact</h1>
+				<p>Let's get in touch!</p>
+				<div className="contact__icon">
+					<a
+						href="https://www.linkedin.com/in/juan-j-perez-payan-9b654b67/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<svg className="contact__icon-item">
+							<use xlinkHref={`#${linked.id}`} />
+						</svg>
+					</a>
 
-          <a
-            href="https://github.com/a2zarslaan"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg className="contact__icon-item">
-              <use xlinkHref={`#${github.id}`} />
-            </svg>
-          </a>
+					<a href="https://github.com/juanjpayan" target="_blank" rel="noopener noreferrer">
+						<svg className="contact__icon-item">
+							<use xlinkHref={`#${github.id}`} />
+						</svg>
+					</a>
 
-          <a
-            href="https://dribbble.com/arslaanamar/shots"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg className="contact__icon-item">
-              <use xlinkHref={`#${dribbble.id}`} />
-            </svg>
-          </a>
+					<a href="https://dribbble.com/arslaanamar/shots" target="_blank" rel="noopener noreferrer">
+						<svg className="contact__icon-item">
+							<use xlinkHref={`#${dribbble.id}`} />
+						</svg>
+					</a>
 
-          <a href="mailto:amar.arslaan@gmail.com" rel="noopener noreferrer">
-            <svg className="contact__icon-item">
-              <use xlinkHref={`#${mail.id}`} />
-            </svg>
-          </a>
+					<a href="mailto:juanj.perez.payan@gmail.com" rel="noopener noreferrer">
+						<svg className="contact__icon-item">
+							<use xlinkHref={`#${mail.id}`} />
+						</svg>
+					</a>
 
-          <a
-            href="https://www.instagram.com/arslaanamar/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg className="contact__icon-item">
-              <use xlinkHref={`#${insta.id}`} />
-            </svg>
-          </a>
-        </div>
-      </Container>
-    </Layout>
-  );
+					<a href="https://www.instagram.com/arslaanamar/" target="_blank" rel="noopener noreferrer">
+						<svg className="contact__icon-item">
+							<use xlinkHref={`#${insta.id}`} />
+						</svg>
+					</a>
+				</div>
+			</Container>
+		</Layout>
+	);
 };
 
 export default ContactPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
 			</Helmet>
 			<Container>
 				<p className="cover">
-					<img src={img_gatsby} alt="Amar Zia Arslaan Being Artsy" />
+					<img src={img_gatsby} alt="Juan bitmoji" />
 				</p>
 				<h1 className="heading">Hello! I'm Juan</h1>
 				<p>Welcome to my personal site.</p>
